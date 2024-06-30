2633b6a33ebe33dd57d564778ccb69b73ab91a1d
DuHyeon Nam <153938155+DuhyunKR@users.noreply.github.com>
2024-06-30 23:29:08 +0900

    Create networkdown.sh

---
+++ network/networkdown.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 도커 다운
+docker-compose -f docker/docker-compose.yaml down -v
+
+# 체인코드 이미지 삭제
+
+# organization 아이덴티티 준비물 삭제
+rm -rf organizations/ordererOrganizations
+rm -rf organizations/peerOrganizations
+
+# genesis.block 삭제
+rm -rf system-genesis-block
+
+# channel 준비물 삭제
