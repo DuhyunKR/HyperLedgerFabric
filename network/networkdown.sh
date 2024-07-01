6b7be897b80db1ad34b6257f7b3e35f32daa8000
DuHyeon Nam <153938155+DuhyunKR@users.noreply.github.com>
2024-07-01 19:25:25 +0900

    Update networkdown.sh

--- network/networkdown.sh
+++ network/networkdown.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # 도커 다운
-docker-compose -f docker/docker-compose.yaml down -v
+IMAGE_TAG=latest docker-compose -f docker/docker-compose.yaml down -v
 
 # 체인코드 이미지 삭제
 
 # organization 아이덴티티 준비물 삭제
 rm -rf organizations/ordererOrganizations
 rm -rf organizations/peerOrganizations
+sudo rm -rf organizations/fabric-ca
 
 # genesis.block 삭제
-rm -rf system-genesis-block
+sudo rm -rf system-genesis-block
 
 # channel 준비물 삭제
 rm -rf channel-artifacts
