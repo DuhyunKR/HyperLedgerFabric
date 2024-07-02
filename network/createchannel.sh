a9bdfea633d4c2d46462a74ec7840eada7a79827
DuHyeon <kndh2914@gmail.com>
2024-07-02 13:40:59 +0900

    Update createchannel.sh

--- network/createchannel.sh
+++ network/createchannel.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-function setOrg1() {
-    export CORE_PEER_TLS_ENABLED=true
-    export CORE_PEER_LOCALMSPID="Org1MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
-    export CORE_PEER_ADDRESS=localhost:7051
-} 
+# function setOrg1() {
+#     export CORE_PEER_TLS_ENABLED=true
+#     export CORE_PEER_LOCALMSPID="Org1MSP"
+#     export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+#     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+#     export CORE_PEER_ADDRESS=localhost:7051
+# } 
 
-function setOrg2 {
-    export CORE_PEER_TLS_ENABLED=true
-    export CORE_PEER_LOCALMSPID="Org2MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
-    export CORE_PEER_ADDRESS=localhost:9051
-}
+# function setOrg2 {
+#     export CORE_PEER_TLS_ENABLED=true
+#     export CORE_PEER_LOCALMSPID="Org2MSP"
+#     export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+#     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+#     export CORE_PEER_ADDRESS=localhost:9051
+# }
+
+. scripts/Utils.sh 
 
 # 0. 환경설정
 export FABRIC_CFG_PATH=${PWD}/config
