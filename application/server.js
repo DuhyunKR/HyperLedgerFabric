11726fe9e15b5e9449691cadfed7f80118da5557
DuHyeon <kndh2914@gmail.com>
2024-07-10 17:57:05 +0900

    Upgrade server.js

--- application/server.js
+++ application/server.js
@@ -14,7 +14,7 @@ const path = require('path')
 const { Gateway, Wallets } = require('fabric-network');
 const ccpPath = path.resolve(__dirname, '..', 'network', 'organizations', 'connection-org1.json')
 let ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf-8'))
-const walletPath = path.join(process.cwd(), 'wallet')
+const walletPath = path.join(process.cwd(), 'wallet');
 // 익스프레스 설정 - bodyparser
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
@@ -35,7 +35,7 @@ app.get('/query', (req, res) => {
 app.post('/car', async (req, res) => {
     // 요청문서에서 param 꺼내기
     const cid = req.body.carid;
-    const make = req.body.carmake;
+    const make = req.body.carmaker;
     const model = req.body.carmodel;
     const color = req.body.carcolor;
     const owner = req.body.carowner;
@@ -88,7 +88,7 @@ app.get('/car', async (req, res) => {
             return;
         }
         // 게이트웨이연결 -> 채널연결 -> 체인코드 객체생성
-        await gateway.connet(ccp, { wallet, identity: 'appUser', discovery: { enabled:true, asLocalhost: true } });
+        await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled:true, asLocalhost: true } });
         const network = await gateway.getNetwork('mychannel');
         const contract = network.getContract('basic');
         // 트랜젝션 제출
@@ -96,15 +96,63 @@ app.get('/car', async (req, res) => {
         console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
         
         // 클라이언트 응답
-        res.send(`Transaction has been evaluated, result is: ${result.toString()}`);
+        res.send(`Transaction has been evaluated, result is: ${result.toString()}`)
     
-    } catch (error){
+    } catch (error) {
         console.error(`Failed to evaluate transaction: ${error}`);
         res.send(`Faild to evaluate transaction: ${error}`);
     } finally {
         await gateway.disconnect();
     }
 })
+
+app.get('/car/histroy', async (req, res) => {
+    
+  // 요청문서에서 param 꺼내기
+  const cid = req.query.carid
+
+  const gateway = new Gateway();
+
+  try {
+      // 지갑생성과 appUser사용자확인
+      const wallet = await Wallets.newFileSystemWallet(walletPath);
+      const identity = await wallet.get('appuUser');
+      if(!identity) {
+          console.log('An identity for the user appUser does not exist');
+          // res.send('An identity for the user appUser does not exist in the wallet');
+          var result_object={};
+          result_object.result="failed"
+          result_object.error=error("An identity for the user appUser does not exist in the wallet")
+          res.send(result_object)
+
+          return;
+      }
+      // 게이트웨이연결 -> 채널연결 -> 체인코드 객체생성
+      await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled:true, asLocalhost: true } });
+      const network = await gateway.getNetwork('mychannel');
+      const contract = network.getContract('basic');
+      // 트랜젝션 제출
+      const result = await contract.evaluateTransaction('GetHistoryCar', cid);
+      console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
+      
+      // 클라이언트 응답
+      // res.send(`Transaction has been evaluated, result is: ${result.toString()}`)
+      var result_object={};
+      result_object.result="success";
+      result_object.content=JSON.parse(result);
+
+      res.send(result_object)
+
+  } catch (error) {
+      console.error(`Failed to evaluate transaction: ${error}`);
+      // res.send(`Faild to evaluate transaction: ${error}`);
+      res.send(error);
+
+  } finally {
+      await gateway.disconnect();
+  }
+})
+
 
 app.listen(port, () => {
   console.log(`Fabcar app listening on port ${port}`)
